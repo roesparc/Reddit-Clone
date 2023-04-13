b26adfbcd39f63b2e1194cefecbdb7d4b7ed68e2
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-13 12:09:28 -0600

    Add module for svgs

--- src/ts_common/modules.d.ts
+++ src/ts_common/modules.d.ts
@@ -2,3 +2,8 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module "*.svg" {
+  import React from "react";
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+}

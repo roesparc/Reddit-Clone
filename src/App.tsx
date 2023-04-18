848713de72a7efc4baf3578c1d44446395b32357
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-18 11:59:16 -0600

    Add user profile to state on authentication change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import { HashRouter } from "react-router-dom";
 import styles from "./styles/App.module.css";
 import Header from "./components/Header";
+import UserAuthModal from "./components/UserAuthModal/UserAuthModal";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
+import getUserProfile from "./functions/getUserProfile";
+import { useAppDispatch } from "./redux/hooks";
+import { updateCurrentUser, updateUserProfile } from "./redux/features/auth";
 
-function App() {
+const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      const userProfile = await getUserProfile(user?.uid);
+      dispatch(updateCurrentUser(user?.uid));
+      dispatch(updateUserProfile(userProfile));
+    });
+  }, [dispatch]);
+
   return (
     <div className={styles.light}>
       <HashRouter>
         <Header />
+        <UserAuthModal />
       </HashRouter>
     </div>
   );
-}
+};
 
 export default App;

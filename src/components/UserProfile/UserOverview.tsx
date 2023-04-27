0f542a2bfe52fddd049095bacad0f2e415db4b21
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-26 21:26:53 -0600

    Fix async bug displaying wrong user posts

--- src/components/UserProfile/UserOverview.tsx
+++ src/components/UserProfile/UserOverview.tsx
@@ -5,20 +5,23 @@ import { getPosts } from "../../functions/getPosts";
 import { Post, UserProfile } from "../../ts_common/interfaces";
 import { INITIAL_POST_DATA } from "../../ts_common/initialStates";
 import styles from "../../styles/userProfile/UserOverview.module.css";
+import { useParams } from "react-router-dom";
 
 interface Props {
   userInfo: UserProfile;
 }
 
 const UserOverView = ({ userInfo }: Props) => {
+  const { username } = useParams();
   const [userPosts, setUserPosts] = useState<Array<Post>>([INITIAL_POST_DATA]);
   const [order, setOrder] = useState<"timestamp" | "upvotes">("timestamp");
 
   useEffect(() => {
-    getPosts("authorId", userInfo.uid, order).then((posts) =>
-      setUserPosts(posts)
-    );
-  }, [userInfo, order]);
+    if (username === userInfo.username)
+      getPosts("authorId", userInfo.uid, order).then((posts) =>
+        setUserPosts(posts)
+      );
+  }, [userInfo, username, order]);
 
   return (
     <div className={styles.root}>

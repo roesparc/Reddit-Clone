fd6e37d3d108792f3780201a9848066dc4340913
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-29 13:14:12 -0600

    Update user profile and theme if the user doc exists in Firestore

--- src/functions/handleAuthStateChange .ts
+++ src/functions/handleAuthStateChange .ts
@@ -3,7 +3,7 @@ import { auth, db } from "../firebase/config";
 import { doc, onSnapshot } from "firebase/firestore";
 import { INITIAL_USER_PROFILE } from "../ts_common/initialStates";
 import { AppDispatch } from "../redux/store";
-import { updateCurrentUser, updateUserProfile } from "../redux/features/auth";
+import { updateUserProfile } from "../redux/features/auth";
 import { setUserTheme } from "../redux/features/theme";
 import { UserProfile } from "../ts_common/interfaces";
 
@@ -15,15 +15,15 @@ const handleAuthStateChange = (
 
   const subscribeToUser = (user: User) => {
     unsubUserSnap = onSnapshot(doc(db, "users", user.uid), (doc) => {
-      dispatch(updateCurrentUser(user.uid));
-      dispatch(updateUserProfile(doc.data() as UserProfile));
-      dispatch(setUserTheme(doc.data()?.userTheme));
+      if (doc.data()) {
+        dispatch(updateUserProfile(doc.data() as UserProfile));
+        dispatch(setUserTheme(doc.data()?.userTheme));
+      }
       completeInitialRender();
     });
   };
 
   const resetUserState = () => {
-    dispatch(updateCurrentUser(undefined));
     dispatch(updateUserProfile(INITIAL_USER_PROFILE));
     dispatch(setUserTheme("light"));
   };

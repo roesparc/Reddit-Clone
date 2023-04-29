25712071aecbdf3485eaa8ff140c1a00d3ab5eee
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-29 13:15:04 -0600

    Delete unused currentUser state

--- src/redux/features/auth.ts
+++ src/redux/features/auth.ts
@@ -4,12 +4,10 @@ import { UserProfile } from "../../ts_common/interfaces";
 import { INITIAL_USER_PROFILE } from "../../ts_common/initialStates";
 
 interface Auth {
-  currentUser: string | undefined;
   userProfile: UserProfile;
 }
 
 const initialState: Auth = {
-  currentUser: undefined,
   userProfile: INITIAL_USER_PROFILE,
 };
 
@@ -17,19 +15,14 @@ const slice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    updateCurrentUser: (state, action: PayloadAction<string | undefined>) => {
-      state.currentUser = action.payload;
-      console.log(state.currentUser);
-    },
     updateUserProfile: (state, action: PayloadAction<UserProfile>) => {
       state.userProfile = { ...action.payload };
     },
   },
 });
 
-export const { updateCurrentUser, updateUserProfile } = slice.actions;
+export const { updateUserProfile } = slice.actions;
 
-export const selectCurrentUser = (state: RootState) => state.auth.currentUser;
 export const selectUserProfile = (state: RootState) => state.auth.userProfile;
 
 export default slice.reducer;

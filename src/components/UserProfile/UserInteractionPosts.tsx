558df67aa49f4900f4497ef15cb125de68486b48
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-30 13:18:28 -0600

    Move infinite scroll logic to custom hook

--- src/components/UserProfile/UserInteractionPosts.tsx
+++ src/components/UserProfile/UserInteractionPosts.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { selectUserProfile } from "../../redux/features/auth";
 import PostsOverview from "../Posts/PostsOverview";
@@ -15,34 +14,8 @@ interface Props {
 const UserInteractionPosts = ({ interactionType }: Props) => {
   const { username } = useParams();
   const userProfile = useAppSelector(selectUserProfile);
-  const {
-    posts,
-    hasMore,
-    isLoading,
-    isCollectionEmpty,
-    fetchInteractionPosts,
-  } = useFetchInteractionPosts(interactionType);
-
-  useEffect(() => {
-    if (posts.length === 0) fetchInteractionPosts();
-  }, [fetchInteractionPosts, posts]);
-
-  useEffect(() => {
-    const onScroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-          document.documentElement.offsetHeight &&
-        hasMore &&
-        !isLoading
-      ) {
-        fetchInteractionPosts();
-      }
-    };
-
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [hasMore, isLoading, fetchInteractionPosts]);
+  const { posts, isLoading, isCollectionEmpty } =
+    useFetchInteractionPosts(interactionType);
 
   return (
     <div className={styles.root}>
--- src/functions/fetchPosts.ts
+++ src/functions/fetchPosts.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useAppSelector } from "../redux/hooks";
 import { selectUserProfile } from "../redux/features/auth";
 import { doc, getDoc } from "firebase/firestore";
@@ -6,17 +6,48 @@ import { db } from "../firebase/config";
 import { Post } from "../ts_common/interfaces";
 import { completePostsInfo } from "./getPosts";
 
+const useInfiniteScroll = (
+  callback: () => void,
+  hasMore: boolean,
+  isLoading: boolean
+) => {
+  const onScroll = useCallback(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight &&
+      hasMore &&
+      !isLoading
+    ) {
+      callback();
+    }
+  }, [hasMore, isLoading, callback]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [onScroll]);
+};
+
+const useFetchFirstBatch = (callback: () => void, posts: Array<Post>) => {
+  useEffect(() => {
+    if (posts.length === 0) callback();
+  }, [callback, posts]);
+};
+
 export const useFetchInteractionPosts = (
   interactionType: "upvotedPosts" | "downvotedPosts" | "savedPosts"
 ) => {
   const userProfile = useAppSelector(selectUserProfile);
-  const interactionPostIds = userProfile[interactionType].slice().reverse();
+  const [interactionPostIds] = useState<Array<string>>(
+    userProfile[interactionType].slice().reverse()
+  );
   const [posts, setPosts] = useState<Array<Post>>([]);
   const [hasMore, setHasMore] = useState<boolean>(true);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [sliceStart, setSliceStart] = useState<number>(0);
   const isCollectionEmpty = interactionPostIds.length < 1;
-  const pageSize = 9;
+  const pageSize = 3;
 
   const fetchInteractionPosts = async () => {
     if (isCollectionEmpty) return;
@@ -37,11 +68,12 @@ export const useFetchInteractionPosts = (
     setIsLoading(false);
   };
 
+  useInfiniteScroll(fetchInteractionPosts, hasMore, isLoading);
+  useFetchFirstBatch(fetchInteractionPosts, posts);
+
   return {
     posts,
-    hasMore,
     isLoading,
     isCollectionEmpty,
-    fetchInteractionPosts,
   };
 };

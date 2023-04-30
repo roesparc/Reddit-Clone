417c359e4d3ca118a651de2c97cd43ac2d80ceec
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-29 21:45:16 -0600

    Add component for empty content with styles

---
+++ src/components/shared/NothingToShow.tsx
@@ -0,0 +1,7 @@
+import styles from "../../styles/shared/NothingToShow.module.css";
+
+const NothingToShow = () => (
+  <h3 className={styles.root}>hmm... looks like there is nothing here</h3>
+);
+
+export default NothingToShow;

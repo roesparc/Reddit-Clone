6d0fe891262bcd1052607cab05a50c5a4583c228
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-05-02 19:54:41 -0600

    Add modules to support png and jpg images

--- src/ts_common/modules.d.ts
+++ src/ts_common/modules.d.ts
@@ -7,3 +7,13 @@ declare module "*.svg" {
   import React from "react";
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
 }
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}

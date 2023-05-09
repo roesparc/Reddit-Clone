45aaf1b1a61b2fd3e52b0d2ed9c8f7cad5927f8c
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-05-08 19:28:14 -0600

    Display loading skeleton & no comments messages

--- src/components/Comments/PostComments.tsx
+++ src/components/Comments/PostComments.tsx
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useFetchComments } from "../../functions/fetchComments";
 import CommentDisplay from "./CommentDisplay";
 import styles from "../../styles/comments/PostComments.module.css";
-import stylesBtn from "../../styles/elements/buttons.module.css";
+import stylesShared from "../../styles/shared/SharedPostsContainer.module.css";
+import LoadingComments from "./LoadingComments";
+import { ImSpinner2 } from "react-icons/im";
+import { IoMdChatbubbles } from "react-icons/io";
+import CommentInput from "./CommentInput";
 
 const PostComments = () => {
   const { postId } = useParams();
-  const [isPostValid, setIsPostValid] = useState<boolean>(false);
   const [order] = useState<"timestamp" | "upvotes">("timestamp");
   const { comments, isLoading, hasMore, isCollectionEmpty } = useFetchComments(
     "postId",
@@ -18,24 +21,41 @@ const PostComments = () => {
 
   return (
     <div className={styles.root}>
-      <div className={styles.commentTextArea}>
-        <textarea rows={1} placeholder="What are your thoughts?"></textarea>
+      <CommentInput />
 
-        <div>
-          <button
-            className={stylesBtn.btnVariantOne}
-            disabled={!isPostValid && true}
-          >
-            Comment
-          </button>
+      {isCollectionEmpty ? (
+        <div className={styles.noCommentsContainer}>
+          <IoMdChatbubbles />
+          <p>No Comments Yet</p>
+          <p>Be the first to share what you think!</p>
         </div>
-      </div>
+      ) : !comments.length ? (
+        <LoadingComments />
+      ) : (
+        <>
+          <div className={styles.commentContainer}>
+            {comments.map((comment) => (
+              <CommentDisplay key={comment.commentId} comment={comment} />
+            ))}
+          </div>
 
-      <div>
-        {comments.map((comment) => (
-          <CommentDisplay key={comment.commentId} comment={comment} />
-        ))}
-      </div>
+          {isLoading && (
+            <ImSpinner2
+              className={stylesShared.loadingPostsSpinner}
+              style={{ marginBottom: "16px" }}
+            />
+          )}
+
+          {!hasMore && comments.length >= 9 && (
+            <p
+              className={stylesShared.noMorePosts}
+              style={{ marginBottom: "16px" }}
+            >
+              No more comments
+            </p>
+          )}
+        </>
+      )}
     </div>
   );
 };
